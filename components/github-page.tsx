c3f4ce8ed92e395c219e48dbeee7531ef0669522
Subash Praveen <subashpraveen@proton.me>
2024-08-21 22:59:00 +0530

    added github redirection

--- components/github-page.tsx
+++ components/github-page.tsx
@@ -2,15 +2,18 @@
 
 import { Github } from "lucide-react";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 export default function GithubPage() {
   return (
     <div>
-      <Button>
-        <span className="mr-2 font-semibold">Github</span>
-        <span className="sr-only">github</span>
-        <Github />
-      </Button>
+      <Link href={"https://github.com/1ofx9/algo-visualizer"}>
+        <Button>
+          <span className="mr-2 font-semibold">Github</span>
+          <span className="sr-only">github</span>
+          <Github />
+        </Button>
+      </Link>
     </div>
   );
 }

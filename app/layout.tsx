8a5551bfdd40f6004004a52af810c0818b9c2b4a
Subash Praveen <subashpraveen@proton.me>
2024-10-27 13:17:26 +0530

    [TEMP] layout update
    - **Temporary** mobile clients will default to desktop view automatically.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/providers/theme/theme-provider";
 import { SortingAlgorithmProvider } from "@/context/visualizer";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <Script id="viewport-adjustment" strategy="beforeInteractive">
+          {`
+            function getCookie(name) {
+              const value = "; " + document.cookie;
+              const parts = value.split("; " + name + "=");
+              if (parts.length === 2) return parts.pop().split(";").shift();
+            }
+            var viewMode = getCookie("view-mode");
+            var viewport = document.querySelector('meta[name="viewport"]');
+            if (viewMode === "desktop") {
+              viewport.setAttribute('content', 'width=1024');
+            } else if (viewMode === "mobile") {
+              viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no');
+            }
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           <SortingAlgorithmProvider>{children}</SortingAlgorithmProvider>
